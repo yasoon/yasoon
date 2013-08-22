dcd280e127f2384ba0ae9b63de65d11839654e6f
Alexander Drozdov <alexander.drozdov@alpari.org>
2013-08-22 16:37:56 +0400

    sp6 added js upload

---
+++ frontend_src/js/lib/helpers.js
@@ -0,0 +1,10 @@
+(function( $ ) {
+
+    $.fn.pressFooter = function () {
+//        var page = $("#page-layout");
+//        if (page.height()) {
+//            $("footer").css('marginTop',page.height());
+//        }
+    };
+
+}( jQuery ));
