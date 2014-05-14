72436a0b4fba2cd4280a361f4cbdd027a219cfe8
timofeymelnik <timofeymelnik@gmail.com>
2014-05-14 16:42:43 +0400

    Fixed post sending

--- frontend_src/Gruntfile.js
+++ frontend_src/Gruntfile.js
@@ -1,109 +1,86 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        'coffee': {
-        	app: {
-				expand: true,
-				flatten: false,
-				cwd: 'assets/coffee',
-				src: ['**/*.coffee'],
-				dest: 'assets/js',
-				ext: '.js'
-        	}
-        },
-		'coffeelint': {
-			options: {
-				force: true
-			},
-			app: 'assets/coffee/{,*/}*.coffee'
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    'coffee': {
+    	app: {
+    		expand: true,
+    		flatten: false,
+    		cwd: 'frontend_src/coffee',
+    		src: ['**/*.coffee'],
+    		dest: 'frontend_src/js',
+    		ext: '.js'
+    	}
+    },
+	'coffeelint': {
+		options: {
+			force: true
 		},
-        'copy': {
-        	js: {
-        		expand: true,
-        		flatten: false,
-        		cwd: 'assets/coffee/',
-        		src: ['templates/*', 'vendor/*'],
-        		dest: 'assets/js/'
-        	},
-        	css: {
-        		expand: true,
-        		flatten: false,
-        		cwd: 'assets/less/',
-        		src: ['fonts/*'],
-        		dest: 'assets/css/'
-        	}
-        },
+		app: 'frontend_src/coffee/{,*/}*.coffee'
+	},
+    'copy': {
+    	js: {
+    		expand: true,
+    		flatten: false,
+    		cwd: 'frontend_src/coffee/',
+    		src: ['templates/*', 'vendor/*'],
+    		dest: 'frontend_src/js/'
+    	},
+    	css: {
+    		expand: true,
+    		flatten: false,
+    		cwd: 'frontend_src/less/',
+    		src: ['fonts/*'],
+    		dest: 'frontend_src/css/'
+    	},
+      build: {
+        expand: true,
+        flatten: false,
+        cwd: 'frontend_src/build/',
+        src: ['**'],
+        dest: 'frontend/'
+      }
+    },
 		'requirejs': {
 			compile: {
 				options: {
-		            mainConfigFile: 'assets/js/config.js',
-		            name: 'config',
-		            out: 'assets/build/js/main.min.js',
-		            optimize : 'none',
-		            baseUrl: 'assets/js',
+          mainConfigFile: 'frontend_src/js/config.js',
+          name: 'config',
+          out: 'frontend_src/build/js/main.min.js',
+          optimize : 'none',
+          baseUrl: 'frontend_src/js',
 				}
 			}
 		},
-        'less': {
+    'less': {
 			development: {
 				options: {
-					paths: ["assets/less"]
+					paths: ["frontend_src/less"]
 				},
 				files: {
-					"assets/build/css/source/source.css": "assets/less/source.less"
+					"frontend_src/build/css/source.css": "frontend_src/less/source.less"
 				}
 			}
-        },
-		'ftp-deploy': {
-			js: {
-				auth: {
-					host: 'yasoon.mnb-t.com',
-					port: 21,
-					authKey: 'yasoon'
-				},
-				src: 'assets/build/js',
-				dest: '/frontend/js',
-			},
-			css: {
-				auth: {
-					host: 'yasoon.mnb-t.com',
-					port: 21,
-					authKey: 'yasoon'
-				},
-				src: 'assets/build/css/source',
-				dest: '/frontend/css',
-			},
-			all: {
-				auth: {
-					host: 'yasoon.mnb-t.com',
-					port: 21,
-					authKey: 'yasoon'
-				},
-				src: 'assets/build/',
-				dest: '/frontend/',
-			}
-		},
-        'watch': {
-            options: {
-                livereload: {
-                    port: 35729,
-                },
-            },
-            scripts: {
-                files: ['assets/coffee/{,*/}*.coffee', 'assets/coffee/{,*/}*.htm'],
-                tasks: ['coffee:app', 'coffeelint', 'copy:js', 'requirejs', 'ftp-deploy:js']
-            },
-            css: {
-                files: ['assets/less/{,*/}*.less'],
-                tasks: ['less', 'copy:css', 'ftp-deploy:css']
-            },
-        },
+    },
+    'watch': {
+      options: {
+        livereload: {
+          port: 35729,
+        }
+      },
+      scripts: {
+        files: ['frontend_src/coffee/{,*/}*.coffee', 'frontend_src/coffee/template/*.htm'],
+        tasks: ['coffee:app', 'coffeelint', 'copy:js', 'requirejs', 'copy:build']
+      },
+      css: {
+        files: ['frontend_src/less/{,*/}*.less'],
+        tasks: ['less', 'copy:css', 'copy:build']
+      },
+    }
     });
 
     grunt.loadNpmTasks('grunt-coffeelint');
-    grunt.loadNpmTasks('grunt-ftp-deploy');
     grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+  	grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
