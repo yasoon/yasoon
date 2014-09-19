794fc74bf332d8cde4a9428c3551fd73cb305e4b
vladyc9 <vladyc9@gmail.com>
2014-09-19 15:55:48 +0300

    return old index view

--- src/Yasoon/Site/Controller/IndexController.php
+++ src/Yasoon/Site/Controller/IndexController.php
@@ -44,7 +44,54 @@ class IndexController {
         $category = $this->category_service->getCategoryList();
         $content = $this->content_service->getAllContent(0);
 
-        $html = $this->render_template('D:\OpenServer\domains\yasoon\src\Yasoon\Site\View\index.php', array('category' => $category, 'content' => $content));
+        $html = "<!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset='utf-8'>
+                    <title>Yasoon</title>
+                    <meta name='viewport' content='width=device-width, initial-scale=0.6, user-scalable=yes' />
+                    <link href='/frontend/img/favicon.ico' rel='shortcut icon' type='image/x-icon' />
+                    <link rel='stylesheet' href='/frontend/css/source.css' type='text/css' media='screen' />
+                    <meta name='description' content='Создание и продвижение Вашего личного профессионального бренда.'>
+                    <script type='text/javascript' src='/frontend/js/vendor/require.js' data-main='/frontend/js/main.min.js'></script>
+
+                    <script>
+                      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                      ga('create', 'UA-53635352-1', 'auto');
+                      ga('send', 'pageview');
+
+                    </script>
+                </head>
+                </html>
+                <body>
+                    <header id='header'></header>
+                    <div class='wrap'>
+                        <div class='main_block'>
+                            <section class='page_content'></section>
+                        </div>
+                    </div>
+                    <footer>
+                        <div class='inside'>
+                            <div class='left aus'>© 2013-2015 yasoon</div>
+                            <nav>
+                                <a href='/#/about'>О нас</a>
+                                <a href='/#/digest'>Дайджест</a>
+                            </nav>
+                            <div class='clearfix'></div>
+                        </div>
+                    </footer>
+                    <script>
+                        Window.config = {
+                            category: ".json_encode($category).",
+                            content: ".json_encode($content).",
+                            userId: false
+                        }
+                    </script>
+                </body>";
 
         return new Response($html);
     }
